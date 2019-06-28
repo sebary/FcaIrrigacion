0325924921a2adc61b95a12c094e4d78dbcfd31b
sebary <sebary@gmail.com>
2019-06-28 19:31:19 -0300

    last but not least

---
+++ RegulatoryEffort.R
@@ -0,0 +1,91 @@
+
+# A Simple Theory of Regulatory Effort with Spillover (Static Version)
+# https://www.rpubs.com/tanglePillowHolder/315910
+
+
+# Cumulative innovation & dynamics
+# http://economics.mit.edu/files/12082
+
+## @knitr SetUpPC2
+RegulatoryEffort <- c("tidyverse","foreach","rootSolve","ggthemes","ggplot2")
+library(rmsfuns)
+load_pkg(RegulatoryEffort)
+
+#library(tidyverse)      # thanks, Hadley
+#library(foreach)        # more elegant loops
+#library(rootSolve)      # utilities for finding zeroes
+#library(ggthemes)       # prettier output options
+
+# Cost relative to effort:
+# C_i(e_i) = Beta_i (e_iˆ2/n + e_i)
+
+# Benefits for improving quality
+#  = log(1 + \frac{e_{i}}{s_{i}} + \gamma_{i} \times e_{j})
+#           - marginal improvement when + spill-over effect
+
+# Total benefit 
+
+# throughout, we will have the endogeneous vector (Effort), e, and
+# the exogenous parameters, th.
+
+# e  is (1. e1, # effort level agent 1 used to calibrate MgCost=MgBenefit
+#        2. e2)
+
+# th is (1. a1,
+#        2. a2,
+#        3. b1,
+#        4. b2,
+#        5. g1, 
+#        6. g2,
+#        7. s1,
+#        8. s2,
+#        9. l)
+
+#   ____________________________________________________________________________
+
+# setting up the functions                                                ####
+## @knitr foc1   
+foc1 <- function (e, th)
+{
+  
+  # step 1: assign -e1 if boundary condition is met
+  if(th[3] > (th[1] * (th[9] / (th[7] * (1 + th[5] * e[2])) + (1 - th[9]) / 2)))
+    return(-e[1])
+  else
+  {
+    # step 2: if boundary condition not met, set MC = MB
+    
+    ch1 <- th[1] * (th[9] / (th[7] * (1 + e[1] / th[7] + th[5] * e[2])))
+    ch2 <- th[1] * (1 - th[9]) / 2
+    ch3 <- th[3] * (1 + e[1])
+    return(ch1 + ch2 - ch3)
+    
+  }
+  
+}
+
+
+foc2 <- function (e, th)
+{
+  
+  # step 1: assign -e2 if boundary condition is met
+  if(th[4] > (th[2] * (th[9] / (th[8] * (1 + th[6] * e[1])) + (1 - th[9]) / 2)))
+    return(-e[2])
+  else
+  {
+    # step 2: if boundary condition not met, set MC = MB
+    
+    ch1 <- th[2] * (th[9] / (th[8] * (1 + e[2] / th[8] + th[6] * e[1])))
+    ch2 <- th[2] * (1 - th[9]) / 2
+    ch3 <- th[4] * (1 + e[2])
+    return(ch1 + ch2 - ch3)
+  }
+  
+}
+
+# wrap it into a model
+## @knitr Model
+model <- function (e, th)
+  c(F1 = foc1(e, th),
+    f2 = foc2(e, th))
+
